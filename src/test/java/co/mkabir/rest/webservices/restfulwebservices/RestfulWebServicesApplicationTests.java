c57e81c793d636fd6142118b821bf049f16c299d
Mursalin Kabir <mail2mursalin@gmail.com>
2022-10-08 22:11:06 +0900

    Initial commit

---
+++ src/test/java/co/mkabir/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mkabir.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
