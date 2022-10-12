86a2cc7d5070ed9c3bc0d33f290769600d87bb23
Mursalin Kabir <mail2mursalin@gmail.com>
2022-10-13 01:38:32 +0900

    Post Method with dynamic url
    implemented post and also returned the uri of the newly created resource

--- src/main/java/co/mkabir/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/co/mkabir/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -13,11 +13,12 @@ public class UserDaoService {
 	// UserDaoService > static list (now)
 
 	private static List<User> users = new ArrayList<>();
+	private static int usersCount =0;
 
 	static {
-		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
-		users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(20)));
 	}
 
 	public List<User> findAll() {
@@ -29,6 +30,11 @@ public class UserDaoService {
 
 		User userfind = users.stream().filter(usr -> usr.getId() == id).findFirst().orElse(null);
 		return userfind;
+	}
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
 	}
 
 }
--- src/main/java/co/mkabir/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/co/mkabir/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,30 +1,47 @@
 package co.mkabir.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
 
-	
 	private UserDaoService service;
-	
+
 	public UserResource(UserDaoService service) {
 		this.service = service;
 	}
-	
-	//Get /users
+
+	// Get /users
 	@GetMapping("/users")
-	public List<User> retrieveAllUsers(){
-		
+	public List<User> retrieveAllUsers() {
+
 		return service.findAll();
 	}
+
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable Integer id){
-		
+	public User retrieveUser(@PathVariable Integer id) {
+
 		return service.findUser(id);
+	}
+
+	// POST /users
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User saveduser=service.save(user);
+		//creating the url for the newly created user
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(saveduser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
