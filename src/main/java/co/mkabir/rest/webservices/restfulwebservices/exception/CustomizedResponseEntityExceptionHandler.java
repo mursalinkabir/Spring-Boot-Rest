dfd85dac8eca93c7c085cc749a20f02ffa469fa9
Mursalin Kabir <mail2mursalin@gmail.com>
2022-11-06 21:07:02 +0900

    validation

--- src/main/java/co/mkabir/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/co/mkabir/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -3,12 +3,14 @@ package co.mkabir.rest.webservices.restfulwebservices.exception;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
+import org.springframework.http.HttpHeaders;
 import co.mkabir.rest.webservices.restfulwebservices.user.UserNotFoundException;
 
 
@@ -32,7 +34,12 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 		
 	}
-
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ErrorDetails errorDetails =	new ErrorDetails(LocalDateTime.now(), "Total Errors:" + ex.getErrorCount() + " First Error:" + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+		return new ResponseEntity<Object>(errorDetails, HttpStatus.BAD_REQUEST);
+	}
 	
 }
 
--- src/main/java/co/mkabir/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/co/mkabir/rest/webservices/restfulwebservices/user/User.java
@@ -2,10 +2,15 @@ package co.mkabir.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
+	@Size(min=2)
 	private String name;
+	@Past
 	private LocalDate birthdate;
 	public User(Integer id, String name, LocalDate birthdate) {
 		super();
